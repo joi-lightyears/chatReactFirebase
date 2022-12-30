b95f62bd179c8ae3c83e164623e9eb8a32231f00
joi-lightyears <kunthanhdat@gmail.com>
2022-12-30 14:22:52 +0700

    fix key code not working on android

--- project/src/components/search.jsx
+++ project/src/components/search.jsx
@@ -21,7 +21,7 @@ const Search = () => {
 
   };
   const HandleKey = (e)=>{
-    e.key === "Enter" && HandleSearch();
+    e.which === 13 && HandleSearch();
   };
   const handleSelect = async()=>{
     const combinedID = currentUser.uid > user.uid ? currentUser.uid + user.uid : user.uid + currentUser.uid;
